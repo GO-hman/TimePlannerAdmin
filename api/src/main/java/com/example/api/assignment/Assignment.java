54073b7980a73426784140df7e99add8e6b53495
PC <ohman.mattias@outlook.com>
2026-07-07 11:53:27 +0200

    added models and controllers for employee and assignment

---
+++ api/src/main/java/com/example/api/assignment/Assignment.java
@@ -0,0 +1,32 @@
+package com.example.api.assignment;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.example.api.employee.Employee;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "assignments")
+@Data
+public class Assignment {
+    
+    @Id
+    @GeneratedValue UUID id;
+    
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "employee_id", nullable=false)
+    @JsonBackReference
+    private Employee employee;
+}
---
+++ api/src/main/java/com/example/api/employee/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.api.employee;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+    
+}
