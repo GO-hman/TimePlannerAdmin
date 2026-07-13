752a55ec661d6c2b07c1965cd5176d856999d875
PC <ohman.mattias@outlook.com>
2026-07-13 13:43:34 +0200

    hardcoded basePath in app.config.ts.

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideDefaultClient } from '../api/providers';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideDefaultClient({ basePath: 'http://localhost:8080' }),
+  ],
 };
