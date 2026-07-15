060cfbcc520885991b58a830f85cd012e06c4d6a
PC <ohman.mattias@outlook.com>
2026-07-15 15:04:03 +0200

    WIP

--- client/src/app/pages/users/users.ts
+++ client/src/app/pages/users/users.ts
@@ -1,10 +1,36 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmployeeControllerService, EmployeeViewOutput } from '../../../api';
+import { firstValueFrom } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
+interface ApiError {
+  status: string;
+  message: string;
+}
 @Component({
   selector: 'app-users',
   imports: [CommonModule],
   templateUrl: './users.html',
   styleUrl: './users.css',
 })
-export class Users {}
+export class Users {
+  private employeeService = inject(EmployeeControllerService);
+
+  employees = signal<EmployeeViewOutput[]>([]);
+  loading = signal<boolean>(false);
+  errors = signal<ApiError | undefined>(undefined);
+
+  async ngOnInit() {
+    this.loading.set(true);
+    try {
+      this.employees.set(await firstValueFrom(this.employeeService.getAll()));
+    } catch (e) {
+      const error = e as HttpErrorResponse;
+      this.errors.set({ message: error.message, status: error.status.toString() });
+      console.log(this.errors());
+    } finally {
+      this.loading.set(false);
+    }
+  }
+}
