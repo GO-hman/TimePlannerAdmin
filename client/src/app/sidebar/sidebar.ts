96a92dd87628be93a6ef68a888f209386012c231
PC <glateohman@gmail.com>
2026-08-20 15:09:52 +0200

    converted old placeholder sidebar to angular material sidebar

--- client/src/app/sidebar/sidebar.ts
+++ client/src/app/sidebar/sidebar.ts
@@ -1,15 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { NavItem } from './nav-item/nav-item';
 import { AuthService } from '../auth/auth.service';
+import { ThemeService } from '../theme/theme.service';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [NavItem],
+  imports: [NavItem, MatSlideToggleModule, MatIconModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
   private auth = inject(AuthService);
+  protected readonly theme = inject(ThemeService);
 
   logout() {
     this.auth.logout();
